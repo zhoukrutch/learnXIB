05b03819e8f4e2c17315a52aa59211d4c10c23b6
zhoukrutch <zhoutao@yidian-inc.com>
2016-08-15 15:27:23 +0800

    git commit

--- learnNavigationController/learnNavigationController/RootViewController.m
+++ learnNavigationController/learnNavigationController/RootViewController.m
@@ -19,28 +19,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.go2NextViewButton setTitle:@"Go to Second View" forState:UIControlStateNormal];
-    
+    //[self setModalTransitionStyle:UIModalTransitionStyleFlipHorizontal];
     //self.title = @"Root View"; //设置navigation title
+    //self.navigationController.hidesBarsOnTap = YES;
+    //self.navigationController.hides
     [self.navigationController.navigationBar setTranslucent:NO]; //设置透明度
     [self.navigationController.navigationBar setBarTintColor:[UIColor whiteColor]]; //设置颜色
     [self.navigationController.navigationBar setTintColor:[UIColor blueColor]];  //navigationItem color
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:nil];
-    //self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:nil];
+    [self.navigationController setToolbarHidden:NO];
+    //self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:nil];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:nil];
+    self.editing = YES;
     [self.go2NextViewButton addTarget:self action:@selector(showSegueAction:)forControlEvents:UIControlEventTouchUpInside];
+    
+    UIImageView *image = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"im"]];
+    [image setFrame:CGRectMake(100, 100, 100, 100)];
+    image.layer.cornerRadius = 3.0;
+    image.clipsToBounds = YES;
+    [self.view addSubview:image];
     
 }
 
 - (void)showSegueAction:(id)sender {
-    SecondViewController *second = [[SecondViewController alloc] init];
-    [self.navigationController pushViewController:second animated:YES];
+    //SecondViewController *second = [[SecondViewController alloc] init];
+    SecondViewController *second = [self.storyboard instantiateViewControllerWithIdentifier:@"SecondViewController"];
     second.title = @"PushViewController";
+    [second setModalTransitionStyle:UIModalTransitionStylePartialCurl];
+    //[self.navigationController pushViewController:second animated:YES];
+    //[self.navigationController showViewController:second sender:sender];
+    //[self showViewController:second sender:sender];
+    
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:second];
+    [self presentViewController:nav animated:YES completion:nil];
 }
 
-//-(void)showSegueAction:(id)sender {
+//- (void)showSegueAction:(id)sender {
 //    [self performSegueWithIdentifier:@"showSegue" sender:sender];
-//    
 //}
 
+
+- (IBAction)unwindForSegueToRootView:(UIStoryboardSegue *)segue{
+    
+}
 
 - (UIButton *)go2NextViewButton {
     if (!_go2NextViewButton) {
